e6c48f983b9b9de1142f2605a513ffa44f1469a6
Evisceration <eviscerationls@gmail.com>
2014-10-29 14:31:55 +0100

    mmc: toggleable software crc
    Change-Id: I79fc427c4f67de0e6a3b8ce60b4b5db0497f1351

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -94,7 +94,7 @@ enum {
  * So we allow it it to be disabled.
  */
 bool use_spi_crc = 1;
-module_param(use_spi_crc, bool, 0);
+module_param(use_spi_crc, bool, 0644);
 
 /*
  * We normally treat cards as removed during suspend if they are not
