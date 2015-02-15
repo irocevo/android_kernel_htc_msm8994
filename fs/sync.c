3a122f25677b7a231333f3478ff02c49186d5e3b
Varun Chitre <varun.chitre15@gmail.com>
2015-02-15 04:36:31 -0500

    Forward port Dynamic Fsync to Linux 3.10
    *. Replace early_suspend with power_suspend
    *. Fixup fsync mechanism to match the new api
    
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -18,7 +18,7 @@
 #include "internal.h"
 #include <trace/events/mmcio.h>
 #ifdef CONFIG_DYNAMIC_FSYNC
-extern bool early_suspend_active;
+extern bool power_suspend_active;
 extern bool dyn_fsync_active;
 #endif
 
@@ -93,6 +93,21 @@ static void fdatawait_one_bdev(struct block_device *bdev, void *arg)
 {
 	filemap_fdatawait(bdev->bd_inode->i_mapping);
 }
+
+#ifdef CONFIG_DYNAMIC_FSYNC
+/*
+ * Sync all the data for all the filesystems (called by sys_sync() and
+ * emergency sync)
+ */
+void sync_filesystems(int wait)
+{
+	iterate_supers(sync_inodes_one_sb, NULL);
+	iterate_supers(sync_fs_one_sb, &wait);
+	iterate_supers(sync_fs_one_sb, &wait);
+	iterate_bdevs(fdatawrite_one_bdev, NULL);
+	iterate_bdevs(fdatawait_one_bdev, NULL);
+}
+#endif
 
 /*
  * Sync everything. We start by waking flusher threads so that most of
@@ -193,7 +208,7 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 	trace_vfs_fsync_done(file);
 	return err;
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !power_suspend_active))
 		return 0;
 	else {
 #endif
@@ -235,7 +250,7 @@ static int do_fsync(unsigned int fd, int datasync)
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !power_suspend_active))
 		return 0;
 	else
 #endif
@@ -315,7 +330,7 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 				unsigned int, flags)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !power_suspend_active))
 		return 0;
 	else {
 #endif
@@ -408,7 +423,7 @@ SYSCALL_DEFINE4(sync_file_range2, int, fd, unsigned int, flags,
 				 loff_t, offset, loff_t, nbytes)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !early_suspend_active))
+	if (likely(dyn_fsync_active && !power_suspend_active))
 		return 0;
 	else
 #endif
